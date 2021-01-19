36fd955285068d92b037b6963a9867bb87ee7653
Juan Cruz <jc23729@gmail.com>
2021-01-19 01:12:45 +0000

    setting up database

---
+++ BasicCRUD.py
@@ -0,0 +1,17 @@
+import os
+from flask import flask
+from flask_sqlalchemy import flask_sqlalchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+###########################
+
+class Puppy(db.Model):
+  
